05b2712237d4f6f439c34c0e547c336a1db39099
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-09-17 16:03:35 -0500

    Add tests to validate user input
    - The tests check if the methods remove_symbols() and set_rotation() work
      by creating CaesarCipher objects with invalid input. This invalid
      input should be converted to valid values.

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 RSpec.describe CaesarCipher do
-  it 'has a version number' do
-    expect(CaesarCipher::VERSION).not_to be nil
+  context %(Validate user text input) do
+    test = CaesarCipher.new('Remove this ? symbol', 13)
+    it %(should discard incompatible chars when creating object) do
+      test.text.to eq('Remove this symbol')
+    end
   end
-
-  
+  context %(Validate user provided shift factor) do
+    test = CaesarCipher.new('Convert to compatible shift factor', 30)
+    it %(should transform number to a compatible shift factor between 1 and 26) do
+      test.rotation.to eq(4)
+    end
+  end
 end
