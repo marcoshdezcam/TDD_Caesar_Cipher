1e5551b917dc44107b107e489667e62088e6e5b2
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-09-18 06:04:43 -0500

    Add test cases for encrypt method
    Test if it encrypts correctly and if it discards unsuported symbols

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,16 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.describe CaesarCipher do
-  context %(Validate user text input) do
+  context %(Validate and filter user text input) do
     test = CaesarCipher.new('Remove this ? symbol', 13)
     it %(should discard incompatible chars when creating object) do
       test.text.to eq('Remove this symbol')
     end
   end
-  context %(Validate user provided shift factor) do
+  context %(Validate and filter user provided shift factor) do
     test = CaesarCipher.new('Convert to compatible shift factor', 30)
     it %(should transform number to a compatible shift factor between 1 and 26) do
       test.rotation.to eq(4)
+    end
+  end
+  context %(Encrypts and decrypts text with a given shift) do
+    it %(Encrypts 'It works' with a shift of 10) do
+      test = CaesarCipher.new('It works', 10)
+      test.encrypt.to eq('Sd gybuc')
+    end
+    it %(Encrypts a message with a shift of 10 and discards symbols) do
+      test = CaesarCipher.new('It works!', 10)
+      test.encrypt.to eq('Sd gybuc')
     end
   end
 end
